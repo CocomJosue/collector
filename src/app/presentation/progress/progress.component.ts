ff3fcea5573a8e2bbea7ab3aef2f331120a1e34c
CocomJosue <jacc11798@gmail.com>
2026-05-05 12:52:59 -0600

    Styles and response

--- src/app/presentation/progress/progress.component.ts
+++ src/app/presentation/progress/progress.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {ProgressSpinnerMode, MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-progress',
   standalone: true,
-  imports: [],
+  imports: [
+    MatProgressSpinnerModule
+  ],
   templateUrl: './progress.component.html',
   styleUrl: './progress.component.css'
 })
 export class ProgressComponent {
-
+  mode: ProgressSpinnerMode = 'determinate';
+  value = 60;
 }
