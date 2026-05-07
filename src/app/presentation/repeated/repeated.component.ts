9fe5b03fb1fdaa5b0e0ee8297d10193bdd3fcce7
CocomJosue <jacc11798@gmail.com>
2026-05-07 16:52:04 -0600

    show repeated stickers list incomplete

--- src/app/presentation/repeated/repeated.component.ts
+++ src/app/presentation/repeated/repeated.component.ts
@@ -4,6 +4,7 @@ import { MatCardModule } from '@angular/material/card';
 import { RouterLink } from "@angular/router";
 import { MatDialog } from '@angular/material/dialog';
 import { RepeatedFormComponent } from '../repeated-form/repeated-form.component';
+import { GROUPS } from '../../drivers/const/const';
 
 @Component({
   selector: 'app-repeated',
@@ -18,9 +19,40 @@ import { RepeatedFormComponent } from '../repeated-form/repeated-form.component'
 })
 export class RepeatedComponent {
   readonly dialog = inject(MatDialog);
+  repeatedList: { key: string, values: number[] }[] = [];
+
+  ngOnInit() {
+    this.loadRepeatedStickersList();
+  }
 
   loadRepeatedStickersList() {
+    this.repeatedList = Object.keys(localStorage)
+    .filter(key => key.startsWith('rep'))
+    .map(key => ({
+      key,
+      values: JSON.parse(localStorage.getItem(key) || '[]')
+    }));
+  }
 
+  getFlag(code: string) {
+    return `https://api.fifa.com/api/v3/picture/flags-sq-1/${code}`;
+  }
+
+  getCountryGroup(code: string) {
+    for (const group of GROUPS) {
+      const country = group.countries.find(
+        c => c.code === code
+    );
+
+    if (country) {
+      return {
+        group: group.letter,
+        name: country.name
+      };
+    }
+  }
+
+    return null;
   }
 
   openDialog(): void {
